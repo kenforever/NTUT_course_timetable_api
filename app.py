6544f130828241e94b40323e04db4933fc36eaf1
kenforever <ken@kenforever.cc>
2022-02-05 03:25:34 +0800

    add_geTable

--- app.py
+++ app.py
@@ -8,14 +8,83 @@ import os
 import time
 from flask_cors import CORS
 from flask_cors import cross_origin
+import base64
+import json
+from Crypto.PublicKey import RSA
+from Crypto import Random
+from Crypto.Hash import SHA
+from Crypto.Signature import PKCS1_v1_5 as PKCS1_signature
+from Crypto.Cipher import PKCS1_v1_5 as PKCS1_cipher
+
 
 app = flask.Flask(__name__)
 CORS(app, cors_allowed_origins='*')
+
+def key_gen():
+    # generate key
+    random_generator = Random.new().read
+    key = RSA.generate(2048, random_generator)
+    # get public key
+    public_key = key.publickey()
+    # get public key in string
+    public_key_string = public_key.exportKey()
+    # get private key in string
+    private_key_string = key.exportKey()
+
+    # get keys location
+    with open("config.json","r") as f:
+        config = json.load(f)
+    private_key_location = config["private_key_location"]
+
+    with open("config.json","r") as f:
+        config = json.load(f)
+    public_key_location = config["public_key_location"]
+
+    # write keys to file
+    with open(public_key_location, "wb") as f:
+        f.write(public_key_string)
+
+    with open(private_key_location, "wb") as f:
+        f.write(private_key_string)
+
+def get_key(key_file):
+    with open(key_file) as f:
+        data = f.read()
+        key = RSA.importKey(data)
+
+    return key
+
+def decrypt(password):
+    # get config file
+    with open("config.json","r") as f:
+        config = json.load(f)
+    private_key_location = config["private_key_location"]
+    # get private key
+    private_key = get_key(private_key_location)
+    # decrypt password
+    decrypted_password = PKCS1_cipher.new(private_key).decrypt(base64.b64decode(password),0).decode('utf-8')
+    return decrypted_password
+
 
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Hello Flask!</h1>"
+
+@app.route('/pubkey', methods=['GET'])
+def pub_key():
+        # get config file
+    with open("config.json","r") as f:
+        config = json.load(f)
+    public_key_location = config["public_key_location"]
+    # get public key
+    with open(public_key_location) as f:
+        pubkey = f.read()
+        
+    pubkey = {"pubkey":pubkey}
+
+    return pubkey
+
 
 @app.route('/geTable', methods=['POST'])
 @cross_origin()
@@ -40,12 +109,13 @@ def result():
 
     FileCreate.geTable(uid,password,year,sem,target)
     TableExchange.Exchange(target)
-    target = target+".json"
-    json_url = os.path.join(app.root_path, target)
+    target_json = target+".json"
+    json_url = os.path.join(app.root_path, target_json)
     data = json.load(open(json_url))
+    os.remove(target)
+    os.remove(target_json)
     return data 
-    os.remove("./table")
-    os.remove(target+".json")
+
 
 # @app.route('/oldTable', methods=['GET'])
 # def old():
@@ -53,6 +123,37 @@ def result():
 #     data = json.load(open(json_url))
 #     return data
 
+@app.route('/sec_geTable', methods=['POST'])
+@cross_origin()
+def sec_getable():
+    if request.is_json == True :
+        content = request.get_json()
+        uid = content["uid"]
+        password = decrypt(content["password"])
+        year = content["year"]
+        sem = content["sem"]
+        target = content["target"]
+        print(password, len(password))
+
+    if target == "":
+        target = uid
+    FileCreate.geTable(uid,password,year,sem,target)
+    TableExchange.Exchange(target)
+    target = target+".json"
+    json_url = os.path.join(app.root_path, target)
+    data = json.load(open(json_url))
+    print(data)
+    return data
     
 if __name__ == '__main__':
+
+
     app.run(host="0.0.0.0", port=8080)
+
+# keys check
+with open("config.json","r") as f:
+    config = json.load(f)
+private_key_location = config["private_key_location"]
+
+if (os.path.isfile(private_key_location) == False):
+    key_gen()
